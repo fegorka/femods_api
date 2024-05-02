b3271086acd4a525eaa13c862b4205977c0f0095
Егорка <101581584+fegorka@users.noreply.github.com>
2024-05-02 18:05:09 +0300

    feat(services): added cuid length & regex to helper service

--- app/services/helper_service.ts
+++ app/services/helper_service.ts
@@ -1,4 +1,6 @@
 export default class HelperService {
+  static cuidLength: number = 24
+  static cuidRegex: RegExp = /[0-9a-km-zA-HJ-NP-Z]+$/
   static trimVersion(version: string): string {
     const segments = version.split('.')
     if (segments.length === 3 && segments[2] === '0') return segments.slice(0, 2).join('.')
