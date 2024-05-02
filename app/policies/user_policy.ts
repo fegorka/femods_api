5e2ac3cdc34046a528560ff132cab2f3f37fb88b
Егорка <101581584+fegorka@users.noreply.github.com>
2024-05-02 18:00:44 +0300

    refactor(policies): whitelist check as been rethought

--- app/policies/user_policy.ts
+++ app/policies/user_policy.ts
@@ -11,12 +11,12 @@ export default class UserPolicy extends BasePolicy {
 
   @allowGuest()
   async show(user: User | null, requestedUser: User): Promise<AuthorizerResponse> {
-    const badUserStatuses: string[] = ['hidden', 'frozen']
+    const allowedStatuses: string[] = ['default']
     const requestedUserStatus = await UserStatus.findByOrFail({ id: requestedUser.userStatusId })
     const userId = user && user.id !== undefined ? user.id : null
     return !(
       userId !== requestedUser.id &&
-      badUserStatuses.includes(requestedUserStatus.name) &&
+      !allowedStatuses.includes(requestedUserStatus.name) &&
       !(await RoleService.userHaveRoleCheck(['extended', 'super'], user))
     )
   }
