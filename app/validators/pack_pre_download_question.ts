f9ca8d7822c0b446897e96ef8840c6922dd5f7b5
Егорка <101581584+fegorka@users.noreply.github.com>
2024-05-06 21:23:04 +0300

    feat(controllers, policies, routes): added packPreDownloadQuestion resource

---
+++ app/validators/pack_pre_download_question.ts
@@ -0,0 +1,59 @@
+import vine from '@vinejs/vine'
+import HelperService from '#services/helper_service'
+
+function storeOrUpdatePackPreDownloadQuestionValidation(
+  packReleaseId: string,
+  packPreDownloadQuestionId: string | null = null
+) {
+  return vine.compile(
+    vine.object({
+      name: vine
+        .string()
+        .trim()
+        .toLowerCase()
+        .minLength(2)
+        .maxLength(32)
+        .regex(/^(?!_)(?!.*__)[a-zA-Z0-9_ ]+(?<!_)(?<!-)$/)
+        .unique(async (db, value): Promise<boolean> => {
+          if (packPreDownloadQuestionId === null)
+            return !(await db
+              .from('pack_pre_download_questions')
+              .where('name', value)
+              .andWhere('pack_release_id', packReleaseId)
+              .first())
+          return !(await db
+            .from('pack_pre_download_questions')
+            .where('name', value)
+            .andWhere('pack_release_id', packReleaseId)
+            .andWhereNot('id', packPreDownloadQuestionId)
+            .first())
+        }),
+      description: vine.string().trim().toLowerCase().minLength(2).maxLength(80).nullable(),
+      packReleaseId: vine
+        .string()
+        .fixedLength(HelperService.cuidLength)
+        .regex(HelperService.cuidRegex)
+        .exists(async (db, value): Promise<boolean> => {
+          return await db.from('pack_releases').where('id', value).first()
+        }),
+    })
+  )
+}
+
+export const preCheckPackPreDownloadQuestionReleaseIdValidator = vine.compile(
+  vine.object({
+    packReleaseId: vine
+      .string()
+      .fixedLength(HelperService.cuidLength)
+      .regex(HelperService.cuidRegex)
+      .exists(async (db, value): Promise<boolean> => {
+        return await db.from('pack_releases').where('id', value).first()
+      }),
+  })
+)
+export const updatePackPreDownloadQuestionValidator = (
+  packReleaseId: string,
+  packPreDownloadQuestionId: string
+) => storeOrUpdatePackPreDownloadQuestionValidation(packReleaseId, packPreDownloadQuestionId)
+export const storePackPreDownloadQuestionValidator = (packReleaseId: string) =>
+  storeOrUpdatePackPreDownloadQuestionValidation(packReleaseId)
