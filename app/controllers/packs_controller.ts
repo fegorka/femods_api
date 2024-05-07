05e4ac3631f263f30bf8382169cbca1dad875d8a
Егорка <101581584+fegorka@users.noreply.github.com>
2024-05-07 17:35:09 +0300

    feat(controllers, validators): added pack special requests

--- app/controllers/packs_controller.ts
+++ app/controllers/packs_controller.ts
@@ -3,27 +3,62 @@ import ControllerService from '#services/controller_service'
 import Pack from '#models/pack'
 import PackPolicy from '#policies/pack_policy'
 import { requestParamsCuidValidator } from '#validators/request'
-import { storePackValidator, updatePackValidator } from '#validators/pack'
+import {
+  indexBySearchPackValidator,
+  indexByTagPackValidator,
+  indexByUserPackValidator,
+  storePackValidator,
+  updatePackValidator,
+} from '#validators/pack'
 import User from '#models/user'
 
 export default class PacksController {
   async index({ auth, bouncer, request }: HttpContext) {
     await ControllerService.authenticateOrSkipForGuest(auth, request)
 
-    if (await bouncer.with(PackPolicy).denies('index'))
-      return Pack.query()
-        .whereHas('packStatus', (packStatusQuery) => {
-          packStatusQuery.whereIn('name', Pack.allowedPackStatusToIndex)
-        })
-        .andWhereHas('packVisibleLevel', (packVisibleLevelQuery) => {
-          packVisibleLevelQuery.whereIn('name', Pack.allowedPackVisibleLevelToIndex)
-        })
-        .andWhereHas('user', (userQuery) => {
-          userQuery.whereHas('userStatus', (userStatusQuery) => {
-            userStatusQuery.whereIn('name', User.allowedUserStatusToIndex)
-          })
-        })
+    if (await bouncer.with(PackPolicy).denies('index')) return this.packIndexWithoutHiddenPacksQuery
     return await Pack.all()
+  }
+
+  async indexBySearch({ auth, bouncer, request, params }: HttpContext) {
+    await ControllerService.authenticateOrSkipForGuest(auth, request)
+
+    await request.validateUsing(indexBySearchPackValidator)
+
+    if (await bouncer.with(PackPolicy).denies('index'))
+      return this.packIndexWithoutHiddenPacksQuery
+        .andWhereILike('name', params.searchString)
+        .orWhereILike('publicName', params.searchString)
+    return Pack.query()
+      .whereILike('name', params.searchString)
+      .orWhereILike('publicName', params.searchString)
+  }
+
+  async indexByTag({ auth, bouncer, request, params }: HttpContext) {
+    await ControllerService.authenticateOrSkipForGuest(auth, request)
+
+    await request.validateUsing(indexByTagPackValidator)
+
+    if (await bouncer.with(PackPolicy).denies('index'))
+      return this.packIndexWithoutHiddenPacksQuery.andWhereHas('tags', (tagsQuery) => {
+        tagsQuery.where('tag_id', params.tagId)
+      })
+    return Pack.query().whereHas('tags', (tagsQuery) => {
+      tagsQuery.where('tag_id', params.tagId)
+    })
+  }
+
+  async indexByUser({ auth, bouncer, request, params }: HttpContext) {
+    await ControllerService.authenticateOrSkipForGuest(auth, request)
+
+    await request.validateUsing(indexByUserPackValidator)
+
+    const userId = auth.user && auth.user.id !== undefined ? auth.user.id : null
+    if (userId === params.userId) return Pack.findManyBy('userId', params.userId)
+
+    if (await bouncer.with(PackPolicy).denies('index'))
+      return this.packIndexWithoutHiddenPacksQuery.andWhere('userId', params.userId)
+    return await Pack.findManyBy({ userId: params.userId })
   }
 
   async store({ bouncer, response, request }: HttpContext) {
@@ -70,4 +105,17 @@ export default class PacksController {
       return response.forbidden('Insufficient permissions')
     return await requestedPack.delete()
   }
+
+  private packIndexWithoutHiddenPacksQuery = Pack.query()
+    .whereHas('packStatus', (packStatusQuery) => {
+      packStatusQuery.whereIn('name', Pack.allowedPackStatusToIndex)
+    })
+    .andWhereHas('packVisibleLevel', (packVisibleLevelQuery) => {
+      packVisibleLevelQuery.whereIn('name', Pack.allowedPackVisibleLevelToIndex)
+    })
+    .andWhereHas('user', (userQuery) => {
+      userQuery.whereHas('userStatus', (userStatusQuery) => {
+        userStatusQuery.whereIn('name', User.allowedUserStatusToIndex)
+      })
+    })
 }
