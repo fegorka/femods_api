fb66ed0a112a4fea93f1aa39cd33df19e92d130f
Егорка <101581584+fegorka@users.noreply.github.com>
2024-05-08 13:00:10 +0300

    feat(controllers): added pagination & search

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -3,13 +3,27 @@ import User from '#models/user'
 import UserPolicy from '#policies/user_policy'
 import ControllerService from '#services/controller_service'
 import { updateUserValidator } from '#validators/user'
-import { requestParamsCuidValidator } from '#validators/request'
+import {
+  requestPageValidator,
+  requestParamsCuidValidator,
+  requestSearchValidator,
+} from '#validators/request'
 
 export default class UsersController {
-  async index({ bouncer, response }: HttpContext) {
+  async index({ bouncer, response, request }: HttpContext) {
+    await request.validateUsing(requestPageValidator)
+    await request.validateUsing(requestSearchValidator)
+
     if (await bouncer.with(UserPolicy).denies('index'))
       return response.forbidden('Insufficient permissions')
-    return await User.all()
+    if (!request.input('search'))
+      return await User.query().paginate(request.input('page'), request.input('limit'))
+    return await User.query()
+      .andWhereILike('name', `%${request.input('search')}%`)
+      .orWhereILike('publicName', `%${request.input('search')}%`)
+      .orWhereILike('id', request.input('search'))
+      .orWhereILike('publicId', request.input('search'))
+      .paginate(request.input('page'), request.input('limit'))
   }
 
   async show({ auth, bouncer, response, request, params }: HttpContext) {
