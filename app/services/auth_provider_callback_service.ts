428880013fd54be58a78e807e2f5645273d75fbd
Егорка <101581584+fegorka@users.noreply.github.com>
2024-06-07 14:33:22 +0300

    feat(services): removed await

--- app/services/auth_provider_callback_service.ts
+++ app/services/auth_provider_callback_service.ts
@@ -1,35 +1,35 @@
-import User from "#models/user";
-import { AccessToken } from "@adonisjs/auth/access_tokens";
-import { SocialProviderNames, SocialProviderDrivers } from "@adonisjs/ally/types";
-import hash from "@adonisjs/core/services/hash";
+import User from '#models/user'
+import { AccessToken } from '@adonisjs/auth/access_tokens'
+import { SocialProviderNames, SocialProviderDrivers } from '@adonisjs/ally/types'
+import hash from '@adonisjs/core/services/hash'
 
 export default class AuthProviderCallBackService {
   static async updateOrCreateUserAndGiveToken(
     provider: SocialProviderNames,
     providerUser: SocialProviderDrivers
   ): Promise<AccessToken> {
-    const providerUserData = await providerUser.user();
+    const providerUserData = await providerUser.user()
 
     const findUserByProvider = {
       provider: provider.toLowerCase(),
-      provider_id: providerUserData.id
-    };
+      provider_id: providerUserData.id,
+    }
 
     const userDetails = {
       name: providerUserData.nickName as string,
       avatar_url: providerUserData.avatarUrl as string,
       provider_id: providerUserData.id as string,
       provider: provider,
-      email: providerUserData.email as string
-    };
-
-    const user = await User.updateOrCreate(findUserByProvider, userDetails);
-
-    if (hash.needsReHash(user.email)) {
-      user.email = await hash.make(providerUserData.email);
-      await user.save();
+      email: providerUserData.email as string,
     }
 
-    return await User.accessTokens.create(user);
+    const user = await User.updateOrCreate(findUserByProvider, userDetails)
+
+    if (hash.needsReHash(user.email)) {
+      user.email = await hash.make(providerUserData.email)
+      await user.save()
+    }
+
+    return await User.accessTokens.create(user)
   }
 }
