ec38dd6aa3597ec6816edb3c9cec75aa9c3dd754
Егорка <101581584+fegorka@users.noreply.github.com>
2024-06-18 12:17:57 +0300

    feat(services): added logic to add data of related models to query by relation names

--- app/services/controller_service.ts
+++ app/services/controller_service.ts
@@ -1,6 +1,9 @@
-import { Authenticator } from '@adonisjs/auth'
 import type { Authenticators } from '@adonisjs/auth/types'
 import type { Request } from '@adonisjs/core/http'
+
+import { Authenticator } from '@adonisjs/auth'
+import { ModelQueryBuilderContract } from '@adonisjs/lucid/types/model'
+
 export default class ControllerService {
   /**
    * @description Used when both guests and authorized users can access to controller
@@ -8,5 +11,27 @@ export default class ControllerService {
    */
   static async authenticateOrSkipForGuest(auth: Authenticator<Authenticators>, request: Request) {
     if (request.header('Authorization') !== undefined) await auth.authenticate()
+  }
+
+  /**
+   * @arg includes Model relation names
+   * @arg queryToModify Model.query()
+   * @description Used to add data of related models to query
+   */
+  static includeRelations(
+    queryToModify: ModelQueryBuilderContract<any>,
+    includes?: string[] | string
+  ) {
+    if (!includes) return queryToModify
+
+    const relationsToInclude: string[] = Array.isArray(includes) ? includes : [includes]
+    let query = queryToModify
+
+    for (const relation of relationsToInclude) {
+      console.log('relation = ' + relation)
+      query = query.preload(relation)
+    }
+
+    return query
   }
 }
