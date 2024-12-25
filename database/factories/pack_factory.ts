a4eba3253e9c2dd9d80752c86bcdef8a9f5ffe14
Егорка <101581584+fegorka@users.noreply.github.com>
2024-12-25 14:12:15 +0300

    fix(factories): removed pack_item_install_path_factory & small fixes

--- database/factories/pack_factory.ts
+++ database/factories/pack_factory.ts
@@ -6,13 +6,11 @@ import { randomInt } from 'node:crypto'
 import PackModCore from '#models/pack_mod_core'
 import { cuid } from '@adonisjs/core/helpers'
 import { PackReleaseFactory } from '#database/factories/pack_release_factory'
-import Tag from '#models/tag'
-import PackStatus from '#models/pack_status'
 
 export const PackFactory = factory
-  .define(Pack, async ({ faker }) => {
-    const truncateString = (str: string, maxLength: number) =>
-      str.length > maxLength ? str.slice(0, maxLength) : str
+  .define(Pack, async () => {
+    //    const truncateString = (str: string, maxLength: number) =>
+    //      str.length > maxLength ? str.slice(0, maxLength) : str
 
     const packVisibleLevels = await PackVisibleLevel.all()
     const packModCores = await PackModCore.all()
