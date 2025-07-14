cd3efd4bbcf9309198a2baba6b350df9ac0b511e
fegorka <101581584+fegorka@users.noreply.github.com>
2025-07-14 10:43:28 +0300

    feat(middleware): added verify by request sign middleware

---
+++ app/middleware/verify_middleware.ts
@@ -0,0 +1,64 @@
+import type { EnvKeysRequestSignSecretRequired } from '#start/env'
+import type { HttpContext } from '@adonisjs/core/http'
+import type { NextFn } from '@adonisjs/core/types/http'
+import env from '#start/env'
+import vine from '@vinejs/vine'
+import { RequestSignService } from '#services/request_sign_service'
+import { MemoryStoreService } from '#services/memory_store_service'
+
+export default class VerifyMiddleware {
+  async handle(
+    { response, request }: HttpContext,
+    next: NextFn,
+    options: { secret: EnvKeysRequestSignSecretRequired } = { secret: 'REQUEST_SIGN_SECRET_SELF' }
+  ) {
+    if (env.get('REQUEST_SIGN_DISABLE_ON_DEVELOPMENT') && env.get('NODE_ENV') === 'development')
+      return await next()
+
+    try {
+      await vine.validate({
+        schema: vine.object({
+          'x-signature-hash': vine.string(),
+          'x-signature-timestamp': vine.number().positive().withoutDecimals(),
+          'x-signature-nonce': vine.string(),
+        }),
+        data: request.headers(),
+      })
+    } catch (error) {
+      throw error
+    }
+
+    const signature = request.header('x-signature-hash')
+    const timestamp = request.header('x-signature-timestamp')
+    const nonce = request.header('x-signature-nonce')
+
+    // timestamp as number correct bc vine.validate guarantees it
+    if (Math.abs(Date.now() - (timestamp as unknown as number)) > 300_000) {
+      return response.status(400).send({ error: 'Invalid timestamp' })
+    }
+
+    if (await MemoryStoreService.has(`reqeust.signature.nonce:${nonce}`)) {
+      return response.status(409).send({ error: 'Nonce already used' })
+    }
+
+    const isValidSignature = RequestSignService.validateRequestSignature(
+      {
+        ...(request.body().data || undefined),
+        ...{
+          timestamp: timestamp,
+          nonce: nonce,
+        },
+      },
+      // signature as string correct bc vine.validate guarantees it
+      signature as string,
+      options.secret
+    )
+
+    if (!isValidSignature) {
+      return response.status(401).send({ error: 'Invalid signature' })
+    }
+
+    await MemoryStoreService.set(`reqeust.signature.nonce:${nonce}`, '1', 300)
+    return await next()
+  }
+}
