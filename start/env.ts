875f95f27d2f607aa70706a45eea72acc8b97015
fegorka <101581584+fegorka@users.noreply.github.com>
2025-07-14 10:27:32 +0300

    feat(env): now env keys is typed

--- start/env.ts
+++ start/env.ts
@@ -11,31 +11,83 @@
 
 import { Env } from '@adonisjs/core/env'
 
-export default await Env.create(new URL('../', import.meta.url), {
+export type EnvKeysAppRequired = keyof typeof envSchemaAppRequired
+const envSchemaAppRequired = {
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.string(),
+}
 
-  /*
-  |----------------------------------------------------------
-  | Variables for configuring database connection
-  |----------------------------------------------------------
-  */
+export type EnvKeysAppOptional = keyof typeof envSchemaAppOptional
+const envSchemaAppOptional = {}
+
+export type EnvKeysRequestSignSecretRequired = keyof typeof envSchemaRequestSignSecretRequired
+const envSchemaRequestSignSecretRequired = {
+  REQUEST_SIGN_SECRET_SELF: Env.schema.string(),
+  REQUEST_SIGN_SECRET_FEMODS: Env.schema.string(),
+  REQUEST_SIGN_SECRET_FEID: Env.schema.string(),
+}
+
+export type EnvKeysRequestSignSecretOptional = keyof typeof envSchemaRequestSignSecretOptional
+const envSchemaRequestSignSecretOptional = {
+  REQUEST_SIGN_DISABLE_ON_DEVELOPMENT: Env.schema.boolean.optional(),
+}
+
+export type EnvKeysDatabaseRequired = keyof typeof envSchemaDatabaseRequired
+const envSchemaDatabaseRequired = {
   DB_HOST: Env.schema.string({ format: 'host' }),
   DB_PORT: Env.schema.number(),
   DB_USER: Env.schema.string(),
-  DB_PASSWORD: Env.schema.string.optional(),
   DB_DATABASE: Env.schema.string(),
+}
 
-  /*
-  |----------------------------------------------------------
-  | Variables for configuring ally package
-  |----------------------------------------------------------
-  */
+export type EnvKeysDatabaseOptional = keyof typeof envSchemaDatabaseOptional
+const envSchemaDatabaseOptional = {
+  DB_PASSWORD: Env.schema.string.optional(),
+}
+
+export type EnvKeysRedisRequired = keyof typeof envSchemaRedisRequired
+const envSchemaRedisRequired = {
+  REDIS_HOST: Env.schema.string({ format: 'host' }),
+  REDIS_PORT: Env.schema.number(),
+  REDIS_PASSWORD: Env.schema.string.optional(),
+}
+
+export type EnvKeysRedisOptional = keyof typeof envSchemaRedisOptional
+const envSchemaRedisOptional = {}
+
+export type EnvKeysDiscordRequired = keyof typeof envSchemaDiscordRequired
+const envSchemaDiscordRequired = {
   DISCORD_CLIENT_ID: Env.schema.string(),
   DISCORD_CLIENT_SECRET: Env.schema.string(),
   GITHUB_CLIENT_ID: Env.schema.string(),
   GITHUB_CLIENT_SECRET: Env.schema.string(),
+}
+
+export type EnvKeysDiscordOptional = keyof typeof envSchemaDiscordOptional
+const envSchemaDiscordOptional = {}
+
+export type EnvKeysAllRequired = keyof typeof envSchemaAllRequired
+const envSchemaAllRequired = {
+  ...envSchemaAppRequired,
+  ...envSchemaDatabaseRequired,
+  ...envSchemaRequestSignSecretRequired,
+  ...envSchemaRedisRequired,
+  ...envSchemaDiscordRequired,
+}
+
+export type EnvKeysAllOptional = keyof typeof envSchemaAllOptional
+const envSchemaAllOptional = {
+  ...envSchemaAppOptional,
+  ...envSchemaDatabaseOptional,
+  ...envSchemaRequestSignSecretOptional,
+  ...envSchemaRedisOptional,
+  ...envSchemaDiscordOptional,
+}
+
+export default await Env.create(new URL('../', import.meta.url), {
+  ...envSchemaAllRequired,
+  ...envSchemaAllOptional,
 })
