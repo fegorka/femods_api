aab19a0d1d01534861bedbe89ca5a03692cb9f4e
fegorka <101581584+fegorka@users.noreply.github.com>
2025-07-19 11:16:04 +0300

    fix(migrations): removed auto-increment ids

--- database/migrations/1713984845519_create_pack_item_pack_pre_download_question_table.ts
+++ database/migrations/1713984845519_create_pack_item_pack_pre_download_question_table.ts
@@ -5,8 +5,6 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
-
       table
         .string('pack_item_id')
         .notNullable()
@@ -19,7 +17,9 @@ export default class extends BaseSchema {
         .references('id')
         .inTable('pack_pre_download_questions')
         .onDelete('CASCADE')
-      table.unique(['pack_item_id', 'pack_pre_download_question_id'])
+      table
+        .unique(['pack_item_id', 'pack_pre_download_question_id'])
+        .primary(['pack_item_id', 'pack_pre_download_question_id'])
     })
   }
 
