03774475340254f9cae4505c234e9faafc7f2813
fegorka <101581584+fegorka@users.noreply.github.com>
2025-07-23 11:30:32 +0300

    feat(env, middleware): now auth middleware may be disable on development

--- app/middleware/auth_middleware.ts
+++ app/middleware/auth_middleware.ts
@@ -1,6 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 import type { Authenticators } from '@adonisjs/auth/types'
+import env from '#start/env'
 
 /**
  * Auth middleware is used authenticate HTTP requests and deny
@@ -14,6 +15,11 @@ export default class AuthMiddleware {
       guards?: (keyof Authenticators)[]
     } = {}
   ) {
+    if (
+      env.get('AUTH_VERIFY_DISABLE_ON_DEVELOPMENT', false) &&
+      env.get('NODE_ENV') === 'development'
+    )
+      return await next()
     await ctx.auth.authenticateUsing(options.guards)
     return next()
   }
