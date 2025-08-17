e5bc524ec2fb7ef95dc30a4106e1e1563765652e
fegorka <101581584+fegorka@users.noreply.github.com>
2025-08-17 20:44:28 +0300

    refactor(services): removed unused console.log

--- app/services/controller_service.ts
+++ app/services/controller_service.ts
@@ -22,15 +22,10 @@ export default class ControllerService {
    * @description Applies sorting to query
    */
   static applySorting(query: ModelQueryBuilderContract<any>, sort: string | undefined) {
-    console.log('0')
-    console.log({ sort_name: sort })
     if (!sort) return query
 
     const direction = sort.startsWith('>') ? 'desc' : 'asc'
     const column = sort.replace(/^[><]/, '')
-
-    console.log('1')
-    console.log({ column_name: column })
 
     return query.orderBy(column, direction)
   }
