ad3a9a89ce888cc3c72bad1bca356b4c65eab2bc
fegorka <101581584+fegorka@users.noreply.github.com>
2025-08-18 16:28:07 +0300

    feat(config, adonisrc): added cache module

--- adonisrc.ts
+++ adonisrc.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     () => import('@adonisjs/core/commands'),
     () => import('@adonisjs/lucid/commands'),
     () => import('@adonisjs/bouncer/commands'),
+    () => import('@adonisjs/cache/commands'),
   ],
 
   /*
@@ -39,6 +40,7 @@ export default defineConfig({
     () => import('@adonisjs/ally/ally_provider'),
     () => import('@adonisjs/bouncer/bouncer_provider'),
     () => import('@adonisjs/redis/redis_provider'),
+    () => import('@adonisjs/cache/cache_provider'),
   ],
 
   /*
